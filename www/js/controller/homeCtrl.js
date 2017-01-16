ee4bb8298b9359f82bf7be10515bd9e46d9b4e6c
todev3 <emerson_abedias@hotmail.com>
2017-01-16 10:02:07 -0200

    versao pre final

--- www/js/controller/homeCtrl.js
+++ www/js/controller/homeCtrl.js
@@ -4,46 +4,24 @@
 //Creates new Controller in "cupom" module
 angular.module('cupom').controller('homeCtrl', function($scope, $rootScope, $cordovaBarcodeScanner, $ionicPopup, cupomApi) {
 
-  // //Save QrCode on server
-  // $scope.saveQr = () => {
-  //
-  //   // hide body
-  //   angular.element(document.querySelector('body')).css('display', 'none');
-  //
-  //   // open camera and scan qrcode
-  //   QRScanner.prepare();
-  //   QRScanner.scan((err, qrCode) => {
-  //
-  //     // show body
-  //     angular.element(document.querySelector('body')).css('display', 'block');
-  //
-  //     // return error
-  //
-  //
-  //     // formats qrcode
-  //     qrCode = qrCode.split('|')[0];
-  //
-  //     // testing
-  //     if(qrCode == 32) alert(qrCode);
-  //     if(qrCode != 32) alert('Erro!!!');
-  //   });
-  //   // show camera
-  //   QRScanner.show();
-  // }
-
   $scope.scanBarcode = function() {
         $cordovaBarcodeScanner.scan().then(function(imageData) {
+
             qrCode = imageData.text.split('|')[0];
+            qrCode = qrCode.replace(/[CFe]{1}/g, '');
 
-            if( qrCode.length == 44 ) {
-              console.log(qrCode);
-              $scope.showAlertSucess();
+            if( qrCode.length == 44 || qrCode.length == 45 ) {
+              cupomApi.postQrCode(qrCode).then(function(result){
+                  $scope.showAlertSucess();
+              }).catch(function(result){
+                  alert("Falha no envio.");
+                });
             }
-            console.log(qrCode);
-            alert(qrCode);
-
+            else {
+                alert("Falha na leitura." );
+            }
         }, function(error) {
-            console.alert("Falha na leitura");
+            console.error("Falha na leitura...");
         });
     };
 
